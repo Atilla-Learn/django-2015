30951320199ebe0d77ff1a142d37151afd41cd06
roodoodey <mathieugrettir@hotmail.com>
2015-12-04 17:30:14 +0000

    added models and admin for this model

--- atillaLearn/web/models.py
+++ atillaLearn/web/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+TYPES_CHOICE = (
+		("conference", "Conférence"), 
+		("training", "Formation"), 
+		("talk", "Talktilla")
+	)
+
 # Create your models here.
+class Item(models.Model):
+	type = models.CharField("Type",choices=TYPES_CHOICE, max_length=30)
+	title = models.CharField("Titre",  max_length=250)
+	date = models.DateField("Date de la présentation", null=True, blank=True)
+	short = models.TextField("Courte description", blank=True)
+	replay_id = models.CharField("Id du replay",  max_length=50, blank=True)
