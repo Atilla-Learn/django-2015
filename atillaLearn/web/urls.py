a4822308202b12df75c6b04df4c1371d801a20c7
thenn42 <thenn4242@gmail.com>
2016-01-13 22:48:09 +0100

    add Talk list view and template

--- atillaLearn/web/urls.py
+++ atillaLearn/web/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-]
+    url(r'^talks$', views.TalkListView.as_view(), name='talks'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- atillaLearn/web/views.py
+++ atillaLearn/web/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from django.views.generic.list import ListView
 
+from .models import Item
 # Create your views here.
 
 def index(request):
     return render_to_response('index.html')
+
+class TalkListView(ListView):
+
+    model = Item
+    queryset = Item.objects.filter(type=Item.TALK)
+    template_name = 'talks.html'
